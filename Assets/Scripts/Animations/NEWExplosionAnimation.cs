a33e6b37e597afb30e0cdcba980892c7835229e1
Hocutt, Taylor G <tghocutt@indiana.edu>
2014-09-23 11:41:58 -0400

    trying to avoid meta files

---
+++ Assets/Scripts/Animations/NEWExplosionAnimation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NEWExplosionAnimation : MonoBehaviour 
+{
+
+	public Animator animator;
+
+	void Start () {
+		animator.Play(0);
+	}
+}
