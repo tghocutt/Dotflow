9f2263a5593ac7ab7c309a91acb429904a0dbe39
Taylor Hocutt <taylor.hocutt@gmail.com>
2014-10-18 15:08:23 -0400

    layed the ground work for menus
    got em drifting on and off screen, am waiting on finalized menu concepts

---
+++ Assets/Scripts/NEW Menu Scripts/DotflowElement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Dotflow
+{
+	public class DotflowElement : MonoBehaviour {
+
+		public Vector3 startScreenPosition;
+		public Vector3 onScreenPosition;
+		public Vector3 offScreenPosition;
+		public float speedMin = 1;
+		public float speedMax = 5;
+		public float desiredTime = 3;
+	}
+}
---
+++ Assets/Scripts/NEW Menu Scripts/DotflowMenu.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Dotflow
+{
+	public class DotflowMenu : MonoBehaviour {
+		
+
+
+		public virtual void Open(DotflowElement[] elements)
+		{
+			foreach (DotflowElement element in elements) 
+			{
+				StartCoroutine(EnterScreen(element));
+			}
+		}
+
+
+		public virtual void Close(DotflowElement[] elements)
+		{
+			foreach (DotflowElement element in elements) 
+			{
+				StartCoroutine(ExitScreen(element));
+			}
+		}
+
+
+		private IEnumerator EnterScreen(DotflowElement element)
+		{
+			element.transform.position = element.startScreenPosition;
+
+			float randy = Random.Range (element.speedMin, element.speedMax);
+			float timeElapsed = 0f;
+
+			while(timeElapsed <= element.desiredTime)
+			{
+				element.transform.localPosition = Vector3.Lerp(element.startScreenPosition, element.onScreenPosition, randy * Time.deltaTime);
+				timeElapsed += Time.deltaTime;
+				yield return null;
+			}
+		}
+
+
+		private IEnumerator ExitScreen(DotflowElement element)
+		{
+			float randy = Random.Range (element.speedMin, element.speedMax);
+
+			float timeElapsed = 0f;
+			
+			while(timeElapsed <= element.desiredTime)
+			{
+				element.transform.localPosition = Vector3.Lerp(element.onScreenPosition, element.offScreenPosition, randy * timeElapsed);
+				timeElapsed += Time.deltaTime;
+				yield return null;
+			}
+		}
+	}
+}
