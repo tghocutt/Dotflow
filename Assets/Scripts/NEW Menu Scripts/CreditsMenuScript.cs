5d0bce1bb621eb4cfc1d69b705f66b2e23fcaf9e
Taylor Hocutt <taylor.hocutt@gmail.com>
2014-10-19 14:59:25 -0400

    started working on hooking up menus
    w00t w00t

---
+++ Assets/Scripts/NEW Menu Scripts/CreditsMenuScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Dotflow
+{
+	public class CreditsMenuScript : DotflowMenu {
+
+		public DotflowElement[] creditsMenuElements = new DotflowElement[0];
+
+		public override void Open(DotflowElement[] elements)
+		{
+			base.Open (elements);
+		}
+
+
+		public override void Close(DotflowElement[] elements)
+		{
+			base.Close (elements);
+		}
+
+		private void MasterMute(GameObject go)
+		{
+
+		}
+
+		private void MusicMute(GameObject go)
+		{
+			
+		}
+
+		private void SFXMute(GameObject go)
+		{
+			
+		}
+
+
+		private void Start () 
+		{
+			UIEventListener.Get (creditsMenuElements[0].gameObject).onClick += MasterMute;
+			UIEventListener.Get (creditsMenuElements[5].gameObject).onClick += MusicMute;
+			UIEventListener.Get (creditsMenuElements[6].gameObject).onClick += SFXMute;
+		}
+	}
+}
