42b36838ecec2e414888e801996183bc447088fa
neal anderson <nealanderson3176952101@149-160-208-34.dhcp-bl.indiana.edu>
2014-10-19 15:00:14 -0400

    Main Menu Script

---
+++ Assets/Scripts/NEW Menu Scripts/MainMenuScript.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Dotflow
+{
+
+	public class MainMenuScript : DotflowMenu {
+
+		public DotflowElement[] mainMenuElements = new DotflowElement[0];
+
+		public override void Open(DotflowElement[] elements)
+		{
+			base.Open (elements);
+		}
+
+
+		public override void Close(DotflowElement[] elements)
+		{
+			base.Close (elements);
+		}
+		
+
+
+		private void Back(GameObject go)
+		{
+
+
+		}
+		
+		private void Settings(GameObject go)
+		{
+			
+			
+		}
+		private void Credits(GameObject go)
+		{
+			
+			
+		}
+		private void Quit(GameObject go)
+		{
+			
+			
+		}
+		private void Menu(GameObject go)
+		{
+			
+			
+		}
+		private void Play(GameObject go)
+		{
+			
+			
+		}
+
+
+
+		private void Start () {
+	
+
+			UIEventListener.Get (mainMenuElements[0].gameObject).onClick += Back;
+			UIEventListener.Get (mainMenuElements[1].gameObject).onClick += Settings;
+			UIEventListener.Get (mainMenuElements[2].gameObject).onClick += Credits;
+			UIEventListener.Get (mainMenuElements[3].gameObject).onClick += Quit;
+			UIEventListener.Get (mainMenuElements[4].gameObject).onClick += Menu;
+			UIEventListener.Get (mainMenuElements[5].gameObject).onClick += Play;
+
+
+
+
+		}
+	}
+}
